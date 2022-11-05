fbc38079f0e53f3d4db16e79f876637b51f6cfe0
aaronsh <aaronsh@bbd.co.za>
2022-11-05 18:00:16 +0200

    Initialized api project.

---
+++ BudgetApi/src/test/java/com/budgetapi/BudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
